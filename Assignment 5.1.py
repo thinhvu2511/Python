7d0169d1d324cbe700b7a7b9a1ed742e5337aeb7
thinhvu2511 <114981431+thinhvu2511@users.noreply.github.com>
2022-10-11 15:22:44 +0700

    Update Assignment 5.1.py

--- Assignment 5.1.py
+++ Assignment 5.1.py
@@ -1,27 +1,55 @@
 #Write a Python program get alerts issued by US-CERT in the current year and write output to CSV and Excel file.
 #Source: https://www.us-cert.gov/ncas/alerts
+
 from bs4 import BeautifulSoup
 import requests
 import csv
 
-listAllert = []
-
 url = 'https://www.us-cert.gov/ncas/alerts'
 
-for i in range(10): # co 10 trang tren web url
-	resp = requests.get(url + "?page=" + str(i))
+def getAllertToFile(url):
+	listsAllert = []
+	resp = requests.get(url)
 	soup = BeautifulSoup(resp.content, "html.parser") #xem sourcecode cua url
-	allert = soup.select('.view-content .item-list .views-field .field-content ') # lay ma so va ten allert
-
+	allert = soup.select('.view-content .item-list .views-field .field-content a') 
+		
+	# lay ma so va ten allert
 	for ele in allert:
-		listAllert.append(ele.text.replace('\n','')) #ma so va ten allert tren url dang o dang text
+		list = []
+		urlAllert = 'https://cisa.gov/uscert' + ele['href']
+		newResp = requests.get(urlAllert)
+		newSoup = BeautifulSoup(newResp.content, 'html.parser')
 
-newLst = []
-for i in range(len(listAllert)):
-	ele = listAllert[i].split(":", 1) # tach chuoi 1 lan vi trong allert co nhieu noi dung ton tai dau :
-	newLst.append(ele)
-	
-with open("allert.csv", mode='w') as file:
-	writer = csv.writer(file)
-	writer.writerow(['CODE','ALLERT'])
-	writer.writerows(newLst)
+		# Collect id of allert
+		idAllert = newSoup.find(id='page-title').text 
+		list.append(idAllert[7:16]) 
+
+		# Collect name of id
+		nameAllert = newSoup.find(id='page-sub-title').text
+		list.append(nameAllert) 
+		
+		# Collect release date and last revised day.
+		date = newSoup.find('div', class_='submitted meta-text').text.strip() 
+		releaseDate = date.replace("Original release date: ", "").strip()
+		if 'Last revised:' in releaseDate:
+			listsdate = releaseDate.split("|")
+			releaseDate = listsdate[0]
+			lastRevised = str(listsdate[1]).replace("Last revised: ", '').strip()
+			list.append(releaseDate)
+			list.append(lastRevised)
+		else:
+			lastRevised = None
+			list.append(releaseDate)
+			list.append(lastRevised)
+			
+		# Add link to list.
+		list.append(urlAllert)
+		listsAllert.append(list)
+
+	# Write file	
+	with open("allert.csv", mode='w') as file:
+		writer = csv.writer(file)
+		writer.writerow(['Alert ID', 'Alert Name', 'Release Date', 'Last revised', 'Alert Link'])
+		writer.writerows(listsAllert)
+
+getAllertToFile(url)
