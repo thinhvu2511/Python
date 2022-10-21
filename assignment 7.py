ac27a9df517063ae0986fec43539a4c130a0b745
thinhvu2511 <114981431+thinhvu2511@users.noreply.github.com>
2022-10-21 13:36:06 +0700

    Update assignment 7.py

--- assignment 7.py
+++ assignment 7.py
@@ -1,6 +1,7 @@
+# Write a program using Flask to CRUD using csv file as database and json as input and response
+
 import json
-from flask import Flask, jsonify, render_template, request
-import pandas as pd
+from flask import Flask, request
 import csv
 from flask_csv import*
 
@@ -13,7 +14,7 @@ class Student:
         self.math = math
     
 
-def read_list_from_csv():
+def read_list_from_csv(): # read data from CSV
     students = []
     with open('test.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
@@ -31,15 +32,11 @@ def write_list_to_csv(students):
 
 @app.route('/')
 def data():
-    # pd.read_csv('test.csv').to_json('student.json')
-    # with open('student.json', 'r') as file:
-    #     students = json.load(file)
-    # return jsonify(students)
     students = read_list_from_csv()
     return json.dumps(students, default=vars)
 
 
-@app.route('/create', methods=['POST'])
+@app.route('/create', methods=['POST']) # Create new data
 def create():
     request_data = request.get_json()
     with open('test.csv', 'a', newline='') as f_object:
@@ -49,7 +46,7 @@ def create():
     return "Create Successfully"
 
 
-@app.route('/update/<int:code>', methods=['POST'])
+@app.route('/update/<int:code>', methods=['POST']) # update data
 def update(code):
     request_data = request.get_json()
     students = read_list_from_csv()
@@ -60,14 +57,14 @@ def update(code):
     return "Update successfully"
 
 
-@app.route('/get/<int:code>')
+@app.route('/get/<int:code>') # get data
 def get(code):
     students = read_list_from_csv()
     get_student = [student for student in students if student.code == str(code)][0]
     return json.dumps(get_student, default=vars)
 
 
-@app.route('/delete/<int:code>', methods=['DELETE'])
+@app.route('/delete/<int:code>', methods=['DELETE']) # delete data
 def delete(code):
     students = read_list_from_csv()
     students.remove([student for student in students if student.code == str(code)][0]) 
@@ -78,8 +75,6 @@ def delete(code):
 if __name__ == '__main__':
     app.run(debug=True)
 
-# # pd.read_json('test.json').to_csv('test.csv')
-# data = pd.read_csv('test.csv')
-# pd.read_csv('test.csv').to_json('new.json')
+
 
 
