652bf926c6c4a562fd5bdd5e48dfb13c1235a5ae
thinhvu2511 <114981431+thinhvu2511@users.noreply.github.com>
2022-10-21 13:40:46 +0700

    Update assignment 8.2.py

--- assignment 8.2.py
+++ assignment 8.2.py
@@ -7,13 +7,14 @@ import requests
 import time
 
 
-slack_Token = 'xoxb-4208909272770-4209010571954-XqwxkwLZIdzQw6l3MOBRkmsH'
-try:
-    while True:
-        client = slack.WebClient(slack_Token)
-        response = requests.get('https://api.thecatapi.com/v1/images/search')
-        data = response.json()
-        client.chat_postMessage(channel='test', text = data[0]['url']) # get URL of cat image
-        time.sleep(5) # Sleep 5 seconds and then do again
-except:
-    print("Disconnect!")
+slack_Token = 'put slack-token in here'
+
+while True:
+    client = slack.WebClient(slack_Token)
+    response = requests.get('https://api.thecatapi.com/v1/images/search')
+    data = response.json()
+    client.chat_postMessage(channel='test', text = data[0]['url']) # get URL of cat image
+    print("Sent.")
+    time.sleep(5) # Sleep 5 seconds and then do again
+
+
