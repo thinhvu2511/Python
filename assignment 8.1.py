979ade6aaf9f47e135b86b2446b6f87b78e5e2de
thinhvu2511 <114981431+thinhvu2511@users.noreply.github.com>
2022-10-21 13:37:04 +0700

    Update assignment 8.1.py

--- assignment 8.1.py
+++ assignment 8.1.py
@@ -8,13 +8,13 @@ import requests
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="$", intents=intents) #use bot command when we start with $
-bot_Token = "MTAyOTU3NDAxOTgyMjkxNTY3NQ.G1FTKI.0nYlEFAKSb6aTU4XUZ4v51LZE7Qz0gXpHrnfSE" #Token of catBot
+bot_Token = "Token of catBot" #Token of catBot
 
 @tasks.loop(seconds=5) # Send messsage each 5 seconds
 async def auto_send():
     response = requests.get('https://api.thecatapi.com/v1/images/search')
     data = response.json()
-    channel = await bot.fetch_channel('1029574834432253984') # ID of channel
+    channel = await bot.fetch_channel('ID of channel') # ID of channel
     await channel.send(data[0]['url'])
 
 @bot.event
